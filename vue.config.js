e910b9f9b29e32fc7a1694a96dfe4aa43840de56
XiaWuSharve <sharve@foxmail.com>
2022-08-17 19:14:51 +0800

    debug

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,15 @@
  * @Author: XiaWuSharve sharve@foxmail.com
  * @Date: 2022-07-20 08:15:46
  * @LastEditors: XiaWuSharve sharve@foxmail.com
- * @LastEditTime: 2022-08-16 14:13:01
+ * @LastEditTime: 2022-08-17 19:11:29
  * @FilePath: \rogra-frontend\vue.config.js
  * @Description: vue配置
  */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sunny-ghost/'
+    : '/',
   transpileDependencies: [
     'vuetify'
   ],
